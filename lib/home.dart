28b5a1df1ccf3bccb6518b3113b98121dd4c94d9
Unperknown <wnsdud105@gmail.com>
2019-09-25 16:12:39 +0900

    [16] Updated : Login Page Done.

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:albarumi/colors.dart';
 
 class AlbarumiApp extends StatelessWidget {
   @override
@@ -11,49 +13,112 @@ class AlbarumiApp extends StatelessWidget {
 
 class AlbarumiHome extends StatefulWidget {
   @override
-  _AlbarumiState createState() => _AlbarumiState();
+  HomeState createState() => HomeState();
 }
 
-class _AlbarumiState extends State<AlbarumiHome> {
+class AlbarumiRegister extends StatefulWidget {
+  @override
+  RegisterState createState() => RegisterState();
+}
+
+class HomeState extends State<AlbarumiHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ListView(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Image.asset('assets/logo.png'),
-                SizedBox(height: 16.0),
-              ],
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: '아이디',
-              ),
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: '비밀번호',
-              ),
-            ),
-            ButtonBar(
-              children: <Widget>[
-                RaisedButton(
-                  child: Text('로그인'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
-            ),
-          ],
+      	body: Center(
+        	child: ListView(
+        	  padding: EdgeInsets.all(40.0),
+        	  children: <Widget>[
+        	    	Column(
+        	    	  children: <Widget>[
+        	    	    SizedBox(height: 240.0),
+        	    	    Text(
+        	    	      '올바르미', 
+        	    	      style: TextStyle(
+        	    	        fontSize: 60.0,
+        	    	        fontFamily: 'TmonMonsori',
+        	    	        color: AlbarumiDarkColor,
+        	    	      ),
+        	    	    ),
+        	    	    SizedBox(height: 200.0),
+        	    	  ],
+        	    	),
+        	    	TextField(
+        	    	  obscureText: true,
+        	    	  decoration: InputDecoration(
+        	    	    border: OutlineInputBorder(
+							borderRadius: new BorderRadius.circular(15.0),
+						),
+        	    	    labelText: '아이디',
+        	    	  ),
+        	    	),
+        	    	SizedBox(height: 15.0),
+        	    	TextField(
+        	    	  obscureText: true,
+        	    	  decoration: InputDecoration(
+        	    	    border: OutlineInputBorder(
+							borderRadius: new BorderRadius.circular(15.0),
+						),
+        	    	    labelText: '비밀번호',
+        	    	  ),
+        	    	),
+					SizedBox(height: 10.0),
+        	    	RaisedButton(
+        	    	    child: Text('로그인'),
+        	    	    onPressed: () {
+        	    	    	Navigator.pop(context);
+        	    	    },
+						textColor: Colors.white,
+						color: AlbarumiDarkColor,
+						shape: RoundedRectangleBorder(
+							borderRadius: new BorderRadius.circular(15.0),
+						),
+						padding: EdgeInsets.symmetric(
+							vertical: 15.0,
+						),
+        	    	),
+        	    	FlatButton(
+        	    	    child: Text('아직 회원이 아니신가요?'),
+        	    	    onPressed: () {
+        	    	        Navigator.push(
+        	    	        	context,
+        	    	          	MaterialPageRoute(builder: (context) => AlbarumiRegister()),
+        	    	        );
+        	    	    },
+					),
+				],
+        	),
         ),
-      ),
-    );
-  }
+      );
+  	}
+}
+
+class RegisterState extends State<AlbarumiRegister> {
+  	@override
+  	Widget build(BuildContext context) {
+    	return Scaffold(
+     		body: Center(
+        		child: ListView(
+          			padding: EdgeInsets.all(40.0),
+          			children: <Widget>[
+						SizedBox(height: 400.0),
+						RaisedButton(
+        	    	    	child: Text('가입하기'),
+        	    	    	onPressed: () {
+        	    	    		Navigator.pop(context);
+        	    	    	},
+							textColor: Colors.white,
+							color: AlbarumiDarkColor,
+							shape: RoundedRectangleBorder(
+								borderRadius: new BorderRadius.circular(15.0),
+							),
+							padding: EdgeInsets.symmetric(
+								vertical: 15.0,
+							),
+        	    		),
+              		],
+            	),
+        	),
+      	);
+  	}
 }
