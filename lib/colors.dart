e23d76c6c001fd1bf76781e9ad25060430f268d2
Unperknown <wnsdud105@gmail.com>
2019-10-07 18:22:43 +0900

    [raw-7] Deleted : All Files

--- lib/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const AlbarumiLightColor = Color(0xFFDCEDC8);
-const AlbarumiMediumColor = Color(0xFF8BC34A);
-const AlbarumiDarkColor = Color(0xFF33691E);
--- lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:albarumi/home.dart';
-
-void main() => runApp(AlbarumiApp());
